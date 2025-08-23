079f04b33e01c0bb84179b76a5e4f30e935d228b
siheartaynagcocode14 <lisondraalleyahheart@gmail.com>
2025-08-23 13:51:11 +0000

    adxbdgfddfg

---
+++ Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        System.out.println("SEATWORK FIVE");
+
+        int countdown = 3;
+        while (countdown > 0) {
+            System.out.println(countdown);
+            countdown--;
+        };
+        
+        
+        System.out.println("SEATWORK SIX");
+        
+        int dice = 1;
+        while (dice < 6) {
+            if (dice < 6) {
+                System.out.println("ANG POGI NG BAGONG HAIRCUT NI SIR LAWRENCE!!!!!!!!!!!1");
+            } else {
+                System.out.println("TECHPRO A3: YESSSSS");
+            }
+            dice = dice + 1;
+        };
+    }
+}
