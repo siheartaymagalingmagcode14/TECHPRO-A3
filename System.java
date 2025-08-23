0d07cdbcb07b144b07315237dd28ead19f0d8453
siheartaynagcocode14 <lisondraalleyahheart@gmail.com>
2025-08-23 13:49:44 +0000

    Abcdefg

---
+++ System.java
@@ -0,0 +1,11 @@
+public class System {
+public static void main(String[] args){
+System.out.println("SEATWORKFIVE");
+int countdown=3;
+while (countdown > 0) {
+    System.out.println(countdown);
+    countdown--;
+}
+System.out.println("HAPPY NEW YEAR!!");
+}
+}
