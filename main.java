8cdf2154c3e385c40c828f84e3634f2354ef68f9
siheartaynagcocode14 <lisondraalleyahheart@gmail.com>
2026-01-08 09:57:39 +0000

    "Sometimes if the code doesnt work thats just life everything doesnt always work the way you want it to be thats why you should try and try again in the end its not how you get the job done but it is the journey thats important."

---
+++ main.java
@@ -0,0 +1,74 @@
+abstract class BankAccount {
+	private String accountNumber;
+	private double balance;
+	
+	public static final String RESET = "\u001B[0m";
+	public static final String BOLD = "\u001B[1m";
+	public static final String GREEN = "\u001B[32m";
+	public static final String BLUE = "\u001B[34m";
+	public static final String RED = "\u001B[31m";
+	public static final String BG_GRAY = "\u001B[47m\u001B[30m";
+
+	public BankAccount(String accountNumber, double initialBalance) {
+		this.accountNumber = accountNumber;
+		this.balance = initialBalance;
+	}
+
+	public abstract void calculateInterest();
+	
+    public double getBalance() {
+        return balance;
+    }
+	
+protected void deposit(double amount) {
+	if (amount > 0) balance += amount;	
+	}
+
+    public void displayAccountInfo() {
+        System.out.println(BG_GRAY + " ACCOUNT ID: " + accountNumber + "" + RESET);
+        System.out.println(BLUE + "Current Balance: $" + balance + RESET);
+    }
+
+    class SavingsAccount extends BankAccount {
+        private double interestRate = 0.05;
+
+        public SavingsAccount(String accountNumber, doubleBalance) {
+            super(accountNumber, balance);
+        }
+
+        @Override
+        public void calculateInterest() {
+            double interest = getBalance() * interestRate;
+            deposit(interest);
+            System.out.println(GREEN + BOLD + "Savings Interest Added: $" + interest + RESET);
+        }
+    }
+
+    class CheckingAccount extends BankAccount {
+        private double fee = 2.50;
+
+        public CheckingAccount(String accountNumber, double balance) {
+            super(accountNumber, balance);
+        }
+
+        @Override
+        public void calculateInterest() {
+            System.out.println(RED + "Checking accounts do not earn interest. Processing fee: $" + fee + RESET);
+        }
+    }
+
+    public class Main {
+        public static void main (String[] args) {
+            BankAccount mySavings = new SavingsAccount("SAV-9910", 1000);
+            BankAccount myChecking = new CheckingAccount("CHK-4452", 500);
+
+            System.out.println("\n" + BankAccount.
+            BOLD + "~~~ BANKING SYSTEM DASHBOARD ~~~ "  + BankAccount.RESET);
+
+            mySavings.displayAccountInfo();
+            mySavings.calculateInterest();
+            System.out.println("Update Savings: $" + mySavings.getBalance());
+
+            System.out.println("----------------");
+
+    
